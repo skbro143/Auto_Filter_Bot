0291ca07a1fd23cc28ee996de3734c0bab0ec955
skbro143 <skbro974323@gmail.com>
2025-10-10 21:28:50 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1563,7 +1563,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ᴅʀᴇᴀᴍxʙᴏᴛᴢ 📜', url='https://github.com/DreamXBotz/Auto_Filter_Bot.git'),
+            InlineKeyboardButton('ꜱᴋᴍᴏᴠɪᴇꜱᴢ 📜', url='https://t.me/SK_MOVIESZ'),
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
